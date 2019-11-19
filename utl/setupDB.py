3cf5466344bf5e498337e7f3483fcf059dc929c3
Manfred <mtan00@stuy.edu>
2019-11-19 14:49:50 -0500

    database initialization

---
+++ utl/setupDB.py
@@ -0,0 +1,23 @@
+#Manfred Tan - Team Jacket
+#SoftDev pd9
+#P01
+#2019-11-19
+
+import sqlite3
+import csv
+
+def createTable():
+    DB_FILE="../database/databases.db"
+
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    # Creates the user database: username|password|decks
+    command = "CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT NOT NULL, decks VARCHAR);"
+    c.execute(command)
+
+    db.commit() #save changes
+    db.close()  #close database
+
+
+createTable()
