b789977a896e7ac0bfe4608fb83f623d68d8414b
Manfred <mtan00@stuy.edu>
2019-11-21 14:50:42 -0500

    database for login

--- app.py
+++ app.py
@@ -9,10 +9,29 @@ import os
 
 ################################################################################################################
 app = Flask(__name__)
+app.secret_key = os.urandom(32) #generates secret key for session
 
 @app.route("/")
 def login():
     return render_template("login.html")
+
+#    if ('username' not in session or 'password' not in session):
+#        redirect(url_for("login"))
+
+
+@app.route("/login", methods=["POST"])
+def login1():
+    session['username'] = request.form["username"]  # assign username key in session to inputted username
+    session['password'] = request.form["password"]  # assign password key in session to inputted password
+    if (session):
+        username = session['username']
+        password = session['password']
+        checkLogin = checkLogin.checkLogin(username, password)
+        if (checkLogin == -1):
+            return render_template('login.html', errorMessage = "Invalid Credentials")
+        return redirect(url_for("home"))
+    else:
+        return render_template('login.html',errorMessage = "")
 
 @app.route("/main")
 def main():
--- database/setupDB.py
+++ database/setupDB.py
@@ -8,28 +8,31 @@ import csv
 
 def createTable():
     DB_FILE="../database/databases.db"
-
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
     # Creates the user database: username|password|decks
-    command = "CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT NOT NULL, decks VARCHAR);"
+    command = "CREATE TABLE IF NOT EXISTS users (userid INTEGER, username TEXT, password TEXT NOT NULL, decks VARCHAR, PRIMARY KEY (userid, username));"
     c.execute(command)
 
     db.commit() #save changes
     db.close()  #close database
 
 
-def tester1():
+def checkLogin(user_username, user_password):
 
-    #pip install -r req.txt
-    #SELECT * FROM users WHERE password='password';
-    
     DB_FILE="../database/databases.db"
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
-    command = ""
+    command = "SELECT * FROM users WHERE username='" + user_username + "' and password='" + user_password + "';"
+    temp = c.execute(command)
+    for row in temp:
+        print(row)
+
 
     db.commit() #save changes
     db.close()  #close database
+
+
+checkLogin('manfred', 'tanx')
